9aa0d7e3a007ca13b0ba1cc7dbecbc7e0efef878
高坤朋 <kunpeng.gao@51job.com>
2019-03-05 18:18:31 +0800

    初始化

---
+++ Assembly/Assembly/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assembly
+//
+//  Created by gkp on 2019/3/5.
+//  Copyright © 2019年 ahu.edu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
